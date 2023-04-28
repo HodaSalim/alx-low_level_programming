6bfd6e91cd854d6d90bb93001d997632b8ec7515
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-28 20:04:56 +0300

    eited lists.h with function declaration

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,8 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#endif // !LISTS_H
+#define LISTS_H
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,3 +18,5 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+
+#endif
