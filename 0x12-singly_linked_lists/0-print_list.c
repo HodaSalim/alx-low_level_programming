c8453a9cca4d7e472d79838246088dff38ec4c3d
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-28 20:52:13 +0300

    added fixes to challnege one and first attempt for challenege 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
 int count = 0;
@@ -9,7 +14,7 @@ while (h)
 {
 	if (h->str == NULL)
 	{
-	    printf("[%d] (nil)\n", h->len);
+	    printf("[0] (nil)\n");
 	}
 	else
 	{
