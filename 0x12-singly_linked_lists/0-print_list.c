599b2541bc6b3072ab09482b972fd55a660c5182
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-28 19:14:43 +0300

    attempt to solve first challenge

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h){
+    int count = 0;
+    while (h) {
+        if (h->str == NULL)
+            printf("[%d] (nil)", h->len );
+        else
+            printf("[%d] %s", h->len, h->str);
+
+        count ++;
+        h = h->next;
+    }
+    return count;
+}
