f4602f5a2a5610808387c2c886992f3282402bca
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-28 20:55:40 +0300

    fixed minor err in challenege 2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
 size_t list_len(const list_t *h)
 {
 int count = 0;
