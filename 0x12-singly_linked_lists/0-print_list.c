c2746a899638822f59d5dc7033308d13ef8d5baf
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-28 19:43:03 +0300

    attempting betty fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,17 +2,21 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
-size_t print_list(const list_t *h){
-    int count = 0;
-    while (h) {
-        if (h->str == NULL)
-            printf("[%d] (nil)\n", h->len );
-        else
-            printf("[%d] %s\n", h->len, h->str);
-
-        count ++;
-        h = h->next;
+size_t print_list(const list_t *h)
+{
+int count = 0;
+while (h)
+{
+    if (h->str == NULL)
+    {
+        printf("[%d] (nil)\n", h->len);
     }
-    return count;
+    else
+    {
+        printf("[%d] %s\n", h->len, h->str);
+    }
+    count++;
+    h = h->next;
+}
+    return (count);
 }
